c829762df633c247a2d9686146f9c209378bc999
satwikbehera12@gmail.com <satwikbehera12@gmail.com>
2024-09-20 03:35:42 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -9,3 +9,7 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def submit_button_click(self, **event_args):
+    # Display a popup that says 'You clicked the button'
+    alert("You clicked the button")
